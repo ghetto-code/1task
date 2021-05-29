b28e8344e24f51d4a587175e8b9a8f92621a55c6
c3po <bettercallanzor@gmail.com>
2021-05-29 22:22:33 +0300

    1_task

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import java.util.*;
+
 class Level1 {
-        public static int squirrel(int N) {
-            if (N <= 0) {
-                return 0;
-            }
-        int fact = N;
-        for (int i = N-1; i > 1; -- i) {
+    public static int squirrel(int N) {
+        if (N <= 1) {
+            return 1;
+        }
+        int fact = 1;
+        for (int i = 2; i < N + 1; i++) {
             fact *= i;
+
         }
         char quant = Integer.toString(fact).charAt(0);
 
