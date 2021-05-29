2dcf44fa7ece383fd49c552bfb2ad87d4e3a51bd
c3po <bettercallanzor@gmail.com>
2021-05-29 20:54:15 +0300

    1_task

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.util.*;
-
 class Level1 {
         public static int squirrel(int N) {
+            if (N <= 0) {
+                return 0;
+            }
         int fact = N;
         for (int i = N-1; i > 1; -- i) {
             fact *= i;
