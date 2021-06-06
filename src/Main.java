d8f9fe2b0547b74b367a16255d2e8c4d7e448c4e
c3po <bettercallanzor@gmail.com>
2021-06-06 21:52:16 +0300

    Merge remote-tracking branch '1/master'

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numb = new int[]{50, 1, 1024};
-        int[] payments = new int[]{20000, 100000, 20000};
+        int[] numb = new int[]{1,3,2};
+        int[] payments = new int[]{1000,500,350};
         int m = numb.length + payments.length;
         int[] xyu = Level1.SynchronizingTables(m,numb,payments);
-
+        for (int x: xyu) {
+            System.out.println(x);
+        }
     }
 }
 
