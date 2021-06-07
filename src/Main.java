2b6ecc19487a267f92c19b3ee5d33b58b4d3aaca
c3po <bettercallanzor@gmail.com>
2021-06-07 22:44:54 +0300

    Uncommitted changes before Update at 06.06.2021, 15:59 [Default Changelist]

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Level1.PatternUnlock(11,new int[]{9,8,7,2,6,5,2,3,4,1,2}));
+        int[] newarr = {10,12};
+        System.out.println(Level2.odometer(newarr));
     }
 }
 
