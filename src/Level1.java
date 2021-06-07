f732c165598987857c47808b3b90182d7ade0031
c3po <bettercallanzor@gmail.com>
2021-06-07 11:45:56 +0300

    Merge remote-tracking branch '1/master'

--- src/Level1.java
+++ src/Level1.java
@@ -1,40 +1,48 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Level1 {
     public static int [] SynchronizingTables(int N, int [] ids, int [] salary) {
         int[] sal = salary;
         int[] id = ids;
+        int[] asd = new int[sal.length + id.length];
+        HashMap<Integer,Integer> newList = new HashMap<>();
         for (int i = 0;  i < sal.length; i ++) {
             for (int j = 0; j < sal.length - 1; j ++) {
-                if(sal[j] < sal[j + 1]){
+                if(sal[j] > sal[j + 1]){
                     int swap = sal[j];
                     sal[j] = sal[j + 1];
                     sal[j + 1] = swap;
                 }
             }
         }
-        int l = 0;
-        int idx = 0;
-        for (int i = 0; i < sal.length; i ++) {
-            int min = id[0];
-            for (int j = l; j < id.length; j ++) {
-                if (id[j] >= min) {
-                    min = id[j];
-                    idx = j;
 
+        for (int i = 0; i < asd.length; i += 2) {
+            asd[i] = sal[i / 2];
+
+        }
+        for (int i = 0; i < asd.length; i += 2) {
+            asd[i+1] = id[i / 2];
+
+        }
+        for(int i = 0; i < asd.length; i += 2) {
+            for(int j = 0; j < asd.length -2; j += 2){
+                if(asd[j + 1] > asd[j + 3]){
+                    int swap = asd[j + 1];
+                    asd[j + 1] = asd[j + 3];
+                    asd[j + 3] = swap;
                 }
             }
-            int swap = sal[idx];
-            sal[idx] = sal[i];
-            sal[i] = swap;
-            l++;
         }
-        for (int i = 0; i < sal.length / 2;i++){
-            int swap = sal[i];
-            sal[i] = sal[sal.length-1 - i];
-            sal[sal.length-1 - i] = swap;
+        for (int i = 0; i < asd.length; i += 2) {
+            newList.put(asd[i + 1], asd[i]);
         }
+        for (int i = 0; i < id.length; i ++){
+            if(newList.containsKey(id[i])){
+                sal[i] = newList.get(id[i]);
+            }
+        }
+
         return sal;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numb = new int[]{1,3,2};
-        int[] payments = new int[]{1000,500,350};
-        int m = numb.length + payments.length;
-        int[] xyu = Level1.SynchronizingTables(m,numb,payments);
-        for (int x: xyu) {
-            System.out.println(x);
-        }
+        int[] ids = new int[]{98,23,61,49,1,79,9};
+        int[] payments = new int[]{1,15,32,47,68,39,24};
+        int m = ids.length + payments.length;
+        int[] xyu = Level1.SynchronizingTables(m,ids,payments);
+            for (int x: xyu) {
+                System.out.println(x);
+            }
     }
 }
 
