d9bf02c72d7a1ec039d8b11af5d0f66a8b2a15b2
c3po <bettercallanzor@gmail.com>
2021-07-04 01:11:23 +0300

    Merge remote-tracking branch 'qwe/master'

--- src/Level1.java
+++ src/Level1.java
@@ -1,41 +1,32 @@
 public class Level1 {
-    public static boolean LineAnalysis(String line){
-
-        String lineCopy = line;
-        String part = String.valueOf(lineCopy.charAt(0));
-        if (line.charAt(0) != '*' || line.charAt(line.length()-1) != '*') {
+    public static boolean MisterRobot(int N, int [] data) {
+        if (N <= 4) {
             return false;
         }
-        if (lineCopy.length() == 1 || lineCopy.length() == 2 || lineCopy.length() == 3) {
-            return true;
-        }
-
-
-        for (int i = 1; i < lineCopy.length(); i ++) {
-            part += lineCopy.charAt(i);
-            if (lineCopy.charAt(i) == '*') {
-                break;
+        for (int i = 0; i < data.length - 2; i ++) {
+            if (!isAscending(data[i], data[i + 1], data[i + 2])) {
+                makeRotation(i, data);
+                if (isAscending(data[i], data[i + 1], data[i + 2])) {
+                    break;
+                }
+                if (i == 2 && !isAscending(data[i], data[i + 1], data[i + 2])) {
+                    return false;
+                }
             }
         }
-        lineCopy = lineCopy.substring(part.length() - 1);
 
-        if(lineCopy.length() == 0) {
-            return true;
-        }
-
-
-        while (lineCopy.length() >= part.length()) {
-            String x = lineCopy.substring(0,part.length());
-            if (!x.equals(part)) {
-                return false;
-            }
-            lineCopy = lineCopy.substring(part.length());
-        }
-        if (lineCopy.length() > 0) {
-            return false;
-        }
 
 
         return true;
+    }
+
+    public static boolean isAscending(int a, int b, int c) {
+        return a < b && b < c;
+    }
+    public static void makeRotation(int count, int [] arr) {
+        int swap = arr[count];
+        arr[count] = arr[count + 1];
+        arr[count + 1] = arr[count + 2];
+        arr[count + 2] = swap;
     }
 }
