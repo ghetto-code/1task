ac2b793e4ea05256796e71f5c5aa00444a884366
c3po <bettercallanzor@gmail.com>
2021-07-12 10:52:14 +0300

    Merge remote-tracking branch 'qwe/master'

--- src/Level1.java
+++ src/Level1.java
@@ -1,26 +1,28 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Level1 {
-    public static int idx = 0;
+
     public static ArrayList<String> combinationsList = new ArrayList<>();
-    public  static String BiggerGreater(String input){
+    public static String BiggerGreater(String input){
         String resultString = "";
-
         int N = input.length();
-
+        if (N < 2) {
+            return resultString;
+        }
         getAllCombinations(input, N*N*N);
         combinationsList.sort(String::compareTo);
-
         for (int i = 0; i < combinationsList.size() - 1; i ++) {
             if (combinationsList.get(i).equals(input) && i != combinationsList.size() - 1) {
                 resultString = combinationsList.get(i + 1);
             }
         }
-
         return resultString;
+
     }
+
     public static void getAllCombinations(String input, int quantity) {
-        if(combinationsList.size() == quantity){
+
+        if(combinationsList.size() == quantity) {
             return;
         }
         if (!combinationsList.contains(input)) {
@@ -28,9 +30,6 @@ public class Level1 {
         } else {
             return;
         }
-
-
-
         ArrayList<String> charArr = new ArrayList<>();
         for (int i = 0; i < input.length() - 1; i ++) {
             char [] arr = input.toCharArray();
@@ -42,6 +41,8 @@ public class Level1 {
         for (String x : charArr) {
             getAllCombinations(x,quantity);
         }
+
     }
+
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        String dontMagicWord = "FFF";
+        String dontMagicWord = "fff";
         System.out.println(Level1.BiggerGreater(dontMagicWord));
 
     }
