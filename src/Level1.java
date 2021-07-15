e9c76a2d43d8b50b2e0c0498d71735fe5a3ded7d
c3po <bettercallanzor@gmail.com>
2021-07-15 14:26:47 +0300

    Merge remote-tracking branch 'qwe/master'

--- src/Level1.java
+++ src/Level1.java
@@ -1,114 +1,80 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;
 
 public class Level1 {
-    public static boolean SherlockValidString(String s) {
-        if (s.length() < 2) {
-            return false;
-        }
-        String rusStr = "aя";
-        String enStr = "az";
-        Range rusRange = new Range(rusStr.charAt(0), rusStr.charAt(1));
-        Range engRange = new Range(enStr.charAt(0), enStr.charAt(1));
-        HashMap<String, Integer> quantity = new HashMap<>();
+    public static String [] TreeOfLife(int H, int W, int N, String [] tree) {
 
-        if (isEng(s, engRange)) {
-            getLetters(s, engRange, quantity);
-        } else {
-            getLetters(s, rusRange, quantity);
-        }
+        // задаем результирующий массив
+        String [] result = new String[H];
 
-        int [] qts = new int[quantity.size()];
-        int id = 0;
+        String [][] clone = new String[H][W];
 
-        for (Map.Entry<String,Integer> x : quantity.entrySet()) {
-            qts[id] = x.getValue();
-            id++;
-        }
-        Arrays.sort(qts);
-        HashMap<Integer, Integer> ints = new HashMap<>();
-        for (Map.Entry<String,Integer> x : quantity.entrySet()) {
-            if (!ints.containsKey(x.getValue())) {
-                ints.put(x.getValue(), 1);
-            } else {
-                ints.put(x.getValue(), ints.get(x.getValue())+1);
-            }
-
-        }
-
-        int [][] intsArr = new int[2][2];
-        int idx = 0;
-        try {
-            for (Map.Entry<Integer, Integer> x : ints.entrySet()) {
-                intsArr[idx][0] = x.getKey();
-                intsArr[idx][1] = x.getValue();
-                idx++;
-            }
-        } catch (ArrayIndexOutOfBoundsException e){
-            return false;
-        }
-        int firstNumber = intsArr[0][0];
-        int firstNumberQuantity = intsArr[0][1];
-        int secondNumber = intsArr[1][0];
-        int secondNumberQuantity = intsArr[1][1];
-        if (ints.size() == 1) {
-            return true;
-        } else if (ints.size() == 2) {
-             if (firstNumberQuantity == 1 && secondNumberQuantity == 1) {
-                 if (firstNumber == 1 || secondNumber == 1){
-                     return true;
-                 }
-                 if (firstNumber - secondNumber == 1 || secondNumber - firstNumber == 1) {
-                    return true;
-                 }
-
-             } else if (firstNumberQuantity == 1) {
-                 if (firstNumber - secondNumber == 1) {
-                     return true;
-                 }
-             } else if (secondNumberQuantity == 1) {
-                 if (secondNumber - firstNumber == 1) {
-                     return true;
-                 }
-             }
-        } else {
-            return false;
-        }
-
-        return false;
-    }
-    public static boolean isEng(String str, Range eng){
-        for (int i = 0; i < str.length(); i++) {
-            if (!eng.contains(str.charAt(i))) {
-                return false;
-            }
-        }
-         return true;
-    }
-    public static void getLetters(String str, Range range, HashMap<String,Integer> qts) {
-        for (int i = 0; i < str.length(); i ++) {
-            if (range.contains(str.charAt(i))) {
-                if (!qts.containsKey(String.valueOf(str.charAt(i)))) {
-                    qts.put(String.valueOf(str.charAt(i)),1);
+        for (int i = 0; i < tree.length; i ++) {
+            for (int j = 0; j < tree[i].length(); j ++) {
+                if (tree[i].charAt(j) == '.') {
+                    clone[i][j] = "0";
                 } else {
-                    qts.put(String.valueOf(str.charAt(i)), qts.get(String.valueOf(str.charAt(i))) + 1);
+                    clone[i][j] = "1";
                 }
 
             }
         }
+
+        for (int years = 0; years < N; years ++) {
+            if (years % 2 != 0) {
+                oneYearAgo(clone);
+                destroy(clone);
+
+            } else {
+                oneYearAgo(clone);
+            }
+        }
+        for (int i = 0; i < clone.length; i ++) {
+            String res = "";
+            for (int j = 0; j < clone[i].length; j++) {
+                if (Integer.valueOf(clone[i][j]) > 0) {
+                    res += "+";
+                } else {
+                    res += ".";
+                }
+            }
+            result[i] = res;
+
+        }
+
+        return result;
     }
 
-}
-class Range {
-    private final int min;
-    private final int max;
-    public Range(int min, int max){
-        this.min = min;
-        this.max = max;
+    public static void oneYearAgo(String[][] tree) {
+        for (int i = 0; i < tree.length; i ++) {
+            for (int j = 0; j < tree[i].length; j ++) {
+                int x = Integer.parseInt(tree[i][j]);
+                tree[i][j] = String.valueOf(++x);
+            }
+        }
     }
-    public boolean contains(int number) {
-        return (number >= min && number <= max);
+    public static void destroy(String[][] tree) {
+        HashMap<Integer[],Integer> destroyPointsHash = new HashMap<>();
+
+        for (int i = 0; i < tree.length; i ++) {
+            for (int j = 0; j < tree[i].length; j++) {
+                if (Integer.parseInt(tree[i][j]) >= 3) {
+
+                    destroyPointsHash.putIfAbsent(new Integer[]{i, j}, 0);
+                    destroyPointsHash.putIfAbsent(new Integer[]{i - 1, j}, 0);
+                    destroyPointsHash.putIfAbsent(new Integer[]{i + 1, j}, 0);
+                    destroyPointsHash.putIfAbsent(new Integer[]{i, j - 1}, 0);
+                    destroyPointsHash.putIfAbsent(new Integer[]{i, j + 1}, 0);
+                }
+            }
+        }
+
+        for (Map.Entry<Integer[],Integer> x : destroyPointsHash.entrySet()) {
+            try {
+                tree[x.getKey()[0]][x.getKey()[1]] = String.valueOf(0);
+            } catch (IndexOutOfBoundsException e) {
+
+            }
+
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        String x = "aavvcc";
-        System.out.println(Level1.SherlockValidString(x));
+
     }
 }
 
